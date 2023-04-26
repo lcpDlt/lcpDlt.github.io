93bbb55d288b144161ab68ec3041982cdabfe8bd
Lcp <1290444712@qq.com>
2023-04-27 00:33:38 +0800

    Site updated: 2023-04-27 00:33:38

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?58e218408b1b190ac9a1adc459c6b6a4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
